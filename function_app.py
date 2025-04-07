b0388fce3cf93031d12f038f1cadba1ce81e49bf
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2025-04-07 00:43:16 -0400

    change

--- function_app.py
+++ function_app.py
@@ -6,7 +6,6 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
     name = req.params.get('name')
     if not name:
         try:
